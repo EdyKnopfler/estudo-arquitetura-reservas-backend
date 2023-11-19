e294632f482598a2fdfeade76c1894c056d44d26
Éderson Cássio <edersoncassio1983@gmail.com>
2023-11-19 17:50:56 -0300

    Configurando elementos da lib e da aplicação de reservas

--- src/main/java/com/derso/reservas/ReservasApplication.java
+++ src/main/java/com/derso/reservas/ReservasApplication.java
@@ -2,8 +2,23 @@ package com.derso.reservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+		"com.derso.reservas.*",
+		"com.derso.controlesessao.*"
+})
+@EntityScan(basePackages = {
+		"com.derso.reservas.*",
+		"com.derso.controlesessao.*"
+})
+@ComponentScan(basePackages = {
+		"com.derso.reservas.*",
+		"com.derso.controlesessao.*"
+})
 public class ReservasApplication {
 
 	public static void main(String[] args) {
